5167bc4d4266c92b74bd8a7c9ad82b956efeabad
crazyboi <dilipkumarsahoo2003@gmail.com>
2023-01-05 15:09:12 +0530

    fa icons link was commented outed so uncommenting

--- js/script.js
+++ js/script.js
@@ -1,25 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // cursor follower
-    // const cursor = document.querySelector('.cursor');
-    // document.addEventListener('mousemove', event => {
-    // cursor.setAttribute("style", "top: " + (event.pageY - 10) + "px; left: " + (event.pageX - 10) + "px;" + "display: block")
-    // });
+  // cursor follower
+  // const cursor = document.querySelector('.cursor');
+  // document.addEventListener('mousemove', event => {
+  // cursor.setAttribute("style", "top: " + (event.pageY - 10) + "px; left: " + (event.pageX - 10) + "px;" + "display: block")
+  // });
 
-    // document.addEventListener('click', () => {
-    //     cursor.classList.add("expand");
+  // document.addEventListener('click', () => {
+  //     cursor.classList.add("expand");
 
-    //     setTimeout(() => {
-    //         cursor.classList.remove("expand");
-    //     }, 500)
-    // });
-    // cursor follower  
+  //     setTimeout(() => {
+  //         cursor.classList.remove("expand");
+  //     }, 500)
+  // });
+  // cursor follower
 
-    document.querySelector("#switch").addEventListener('click', () => {
-        // console.log(document.getElementById('switchTheme').getElementsByTagName('small'));
-        document.body.classList.toggle("switchNow");
-    });
-    const contactModal = document.querySelector("#contactModal")
-    contactModal.addEventListener("click", () => {
-        contactModal.innerHTML = "LOL";
-    });
+  document.querySelector("#switch").addEventListener("click", () => {
+    // console.log(document.getElementById('switchTheme').getElementsByTagName('small'));
+    document.body.classList.toggle("switchNow");
+  });
+
+  //Contact click Modal
+  const openModalButton = document.querySelector("#open-modal");
+  const modalWindowOverlay = document.querySelector("#modal-overlay");
+  const crossModal = document.querySelector(".cross-modal");
+
+  //showModal
+  const showModalWindow = () => {
+    modalWindowOverlay.style.display = "flex";
+    // Enable scroll
+    // document.body.style.overflow = "hidden";
+  };
+  openModalButton.addEventListener("click", showModalWindow);
+
+  // hideModal
+  const hideModalWindow = () => {
+    modalWindowOverlay.style.display = "none";
+    // disable scroll
+
+    // document.body.style.overflow = "auto";
+  };
+  crossModal.addEventListener("click", hideModalWindow);
+
+  // Hide On Overlay
+  const hideModalWindowOnBlur = (event) => {
+    if (event.target === event.currentTarget) {
+      console.log(event.target === event.currentTarget);
+      hideModalWindow();
+    }
+  };
+  modalWindowOverlay.addEventListener("click", hideModalWindowOnBlur);
 });
